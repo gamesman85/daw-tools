db564c0f85c1c063863eb9015fcb0b067895627d
Daniel Andreas Wang <daniel.andreas.wang@gmail.com>
2021-08-28 11:20:00 +0200

    Add optional speech to download

--- download.py
+++ download.py
@@ -1,13 +1,22 @@
 #!python3
 
 import requests
+from speak import *
 
-def download(source, target):
-  print('Downloading ' + source + ' to ' + target + '...')
+def download(source, target, sound):
+  txt = 'Downloading ' + source + ' to ' + target + '...'
+  print(txt)
+  if sound:
+    speak(txt)
+  
   res = requests.get(source)
   res.raise_for_status()
   f = open(target, 'wb')
   for chunk in res.iter_content(100000):
     f.write(chunk)
   f.close()
-  print('Download finished.')
+  
+  txt = 'Download finished.'
+  print(txt)
+  if sound:
+    speak(txt)
